087d5bcc4508e350264b55563ecb0a6aa7cbc6f6
bswearingen11119 <bswearingen@mcw.edu>
2022-11-10 10:15:32 -0600

    Added min/max and 4d viewer
    Added min/max slider to 3d viewer.
    Added 4d viewer with slider for bin selection.

--- JupyterViewers.py
+++ JupyterViewers.py
@@ -72,11 +72,14 @@ class ImageSliceViewer3D:
     
     """
     
-    def __init__(self, volume,mini,maxi,figsize=(8,8), cmap='gray',):
+    def __init__(self, volume,mini,maxi,figsize=(8,8), cmap='gray',step_size=1.0):
         self.volume = volume
         self.figsize = figsize
         self.cmap = cmap
         self.v = [mini, maxi]
+        #guarantee minimum number of steps
+        tst=(maxi-mini)/20
+        self.step_size=np.min([step_size,tst])
         
         # Call to select slice plane
         ipyw.interact(self.view_selection, view=ipyw.RadioButtons(
@@ -93,14 +96,68 @@ class ImageSliceViewer3D:
         # Call to view a slice within the selected slice plane
         ipyw.interact(self.plot_slice, 
             z=ipyw.IntSlider(min=0, max=maxZ, step=1, continuous_update=True, 
-            description='Image Slice:'))
+            description='Image Slice:'),
+            m=ipyw.FloatRangeSlider(min=self.v[0],max=self.v[1],
+                    readout_format='1.3f',value=[self.v[0],self.v[1]],
+                    step=self.step_size, description='Disp Rng:',
+                    layout={'width':'350px'}))
         
-    def plot_slice(self, z):
+    def plot_slice(self, z, m):
         # Plot slice for the given plane and slice
         self.fig = plt.figure(figsize=self.figsize)
         plt.imshow(self.vol[:,:,z], cmap=plt.get_cmap(self.cmap), 
-            vmin=self.v[0], vmax=self.v[1])
+            vmin=m[0], vmax=m[1])
+            
+            
+class ImageSliceViewer4D:
+    """
+    ImageSliceViewer4D is for viewing volumetric image slices and bins/timepoints in jupyter or
+    ipython notebooks.
+    
+    User can interactively change the slice and bin/timepoint plane selection for the image and
+    the slice plane being viewed.
 
+    Argumentss:
+    Volume = 4D input image
+    figsize = default(8,8), to set the size of the figure
+    cmap = default('plasma'), string for the matplotlib colormap. You can find
+    more matplotlib colormaps on the following link:
+    https://matplotlib.org/users/colormaps.html
+    
+    """
+    
+    def __init__(self, volume,mini,maxi,figsize=(8,8), cmap='gray',):
+        self.volume = volume
+        self.figsize = figsize
+        self.cmap = cmap
+        self.v = [mini, maxi]
+        # Call to select slice plane
+        ipyw.interact(self.view_selection, view=ipyw.RadioButtons(
+            options=['x-y','y-z', 'z-x'], value='x-y',
+            description='Slice plane selection:', disabled=False,
+            style={'description_width': 'initial'}))
+    
+    def view_selection(self, view):
+        # Transpose the volume to orient according to the slice plane selection
+        orient = {"y-z":[1,2,0,3], "z-x":[2,0,1,3], "x-y": [0,1,2,3]}
+        self.vol = np.transpose(self.volume, orient[view])
+        maxZ = self.vol.shape[2] - 1
+        maxT = self.vol.shape[3] - 1
+        
+        # Call to view a slice within the selected slice plane
+        ipyw.interact(self.plot_slice,
+            z=ipyw.IntSlider(min=0, max=maxZ, step=1, continuous_update=True,
+            description='Image Slice:'),
+            t=ipyw.IntSlider(min=0, max=maxT,step=1, continuous_upate=True,
+                               description='Timepoint/Bin:'))
+        
+    def plot_slice(self, z, t):
+        # Plot slice for the given plane and slice
+        self.fig = plt.figure(figsize=self.figsize)
+        plt.imshow(self.vol[:,:,z,t], cmap=plt.get_cmap(self.cmap),
+            vmin=self.v[0], vmax=self.v[1]) #self.v[1])
+            
+            
 class ImageTraceViewerSingle:
     
     def __init__(self, volume,mini,maxi,figsize=(8,8), cmap='gray',):
